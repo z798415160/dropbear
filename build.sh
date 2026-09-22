51b6e9ac201e57daf42210b1b2fb7f5e3e205bd8
z798415160 <798415160@qq.com>
2026-09-22 13:27:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ fi
 workdir="$(pwd)"
 builddir="$(mktemp -d)"
 trap 'rm -rf "$builddir"' EXIT
-apt-get install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+sudo apt-get install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 
 zig_pkg_arch=`uname -m`
 zig_pkg="zig-${zig_pkg_arch}-linux-${ZIG_VERSION}.tar.xz"
